9d2a6f8ec92066e1bef8de319fc4c5f044477e17
Vanesa Osorio <vanesa@ridinghoodmedia.com>
2024-05-29 14:45:18 -0500

    New changes Changes to be committed: 	modified:   src/routes/+layout.svelte 	modified:   src/routes/styles.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,63 @@
 <script>
 	import Header from './Header.svelte';
 	import './styles.css';
+
+	//const sampleImages = [
+	//	{
+	//		url: 'https://images.pexels.com/photos/19168399/pexels-photo-19168399/free-photo-of-mar-blanco-y-negro-moda-playa.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/2076966/pexels-photo-2076966.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/6932086/pexels-photo-6932086.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/19290532/pexels-photo-19290532/free-photo-of-moda-gafas-de-sol-gente-mujer.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/19168405/pexels-photo-19168405/free-photo-of-mar-blanco-y-negro-puesta-de-sol-moda.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/17758900/pexels-photo-17758900/free-photo-of-moda-gente-mujer-oscuro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/17839253/pexels-photo-17839253/free-photo-of-moda-gente-mujer-relajacion.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/17758817/pexels-photo-17758817/free-photo-of-moda-playa-gente-mujer.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/19290540/pexels-photo-19290540/free-photo-of-moda-gente-mujer-relajacion.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/18557754/pexels-photo-18557754/free-photo-of-moda-hombre-amor-gente.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/17747044/pexels-photo-17747044/free-photo-of-paisaje-moda-hombre-gafas-de-sol.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/17758670/pexels-photo-17758670/free-photo-of-moda-hombre-gente-mujer.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/17758720/pexels-photo-17758720/free-photo-of-moda-amor-gente-mujer.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/17765099/pexels-photo-17765099/free-photo-of-moda-hombre-gente-mujer.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/17771152/pexels-photo-17771152/free-photo-of-blanco-y-negro-moda-hombre-gafas-de-sol.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/17838930/pexels-photo-17838930/free-photo-of-ligero-moda-hombre-gente.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/17838918/pexels-photo-17838918/free-photo-of-moda-hombre-gente-mujer.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	},
+	//	{
+	//		url: 'https://images.pexels.com/photos/18557760/pexels-photo-18557760/free-photo-of-paisaje-moda-hombre-persona.jpeg?auto=compress&cs=tinysrgb&w=400'
+	//	}
+	//];
 </script>
 
 <div class="app">
